061af781432b38bd021269a3b8650fcb4fd84985
ThunderstarVT <155100050+ThunderstarVT@users.noreply.github.com>
2026-03-16 14:24:43 +0100

    fix: particle count setting applies to bursts

--- Assets/Scripts/ParticleEmissionModifier.cs
+++ Assets/Scripts/ParticleEmissionModifier.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Singletons;
@@ -6,13 +5,24 @@ using UnityEngine;
 
 public class ParticleEmissionModifier : MonoBehaviour
 {
-    private Dictionary<ParticleSystem, float> particleSystems = new();
+    private Dictionary<ParticleSystem, float> particleSystemEmissionRates = new();
+    private Dictionary<ParticleSystem, List<(float, float, float)>> particleSystemEmissionBursts = new();
 
     private void Start()
     {
         foreach (ParticleSystem p in GetComponents<ParticleSystem>())
         {
-            particleSystems.Add(p, p.emission.rateOverTimeMultiplier);
+            particleSystemEmissionRates.Add(p, p.emission.rateOverTimeMultiplier);
+            
+            particleSystemEmissionBursts.Add(p, new List<(float, float, float)>());
+
+            for (int i = 0; i < p.emission.burstCount; i++)
+            {
+                particleSystemEmissionBursts[p].Add((
+                    p.emission.GetBurst(i).count.constantMin, 
+                    p.emission.GetBurst(i).count.constantMax, 
+                    p.emission.GetBurst(i).count.constant));
+            }
         }
 
         OnApply();
@@ -27,10 +37,28 @@ public class ParticleEmissionModifier : MonoBehaviour
 
     private void OnApply()
     {
-        particleSystems.Keys.ToList().ForEach(p =>
+        particleSystemEmissionRates.Keys.ToList().ForEach(p =>
         {
+            bool wasPlaying = p.isPlaying;
+            p.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            
             ParticleSystem.EmissionModule emission = p.emission;
-            emission.rateOverTimeMultiplier = particleSystems[p] * SettingsManager.Instance.ParticleRateModifier;
+            emission.rateOverTimeMultiplier = particleSystemEmissionRates[p] * SettingsManager.Instance.ParticleRateModifier;
+            
+            for (int i = 0; i < particleSystemEmissionBursts[p].Count; i++)
+            {
+                ParticleSystem.Burst burst = emission.GetBurst(i);
+                ParticleSystem.MinMaxCurve curve = burst.count;
+                curve.constantMin = particleSystemEmissionBursts[p][i].Item1 * SettingsManager.Instance.ParticleRateModifier;
+                curve.constantMax = particleSystemEmissionBursts[p][i].Item2 * SettingsManager.Instance.ParticleRateModifier;
+                curve.constant = particleSystemEmissionBursts[p][i].Item3 * SettingsManager.Instance.ParticleRateModifier;
+                burst.count = curve;
+                emission.SetBurst(i, burst);
+                
+                Debug.Log(emission.GetBurst(i).count.constantMax);
+            }
+            
+            if (wasPlaying) p.Play();
         });
     }
 }
